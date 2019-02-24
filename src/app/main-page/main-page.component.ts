c5009f76ae52a14b35692a889ebc0acdbd342630
nikitaChihranov <nikita.chihranov@gmail.com>
2019-02-25 03:28:39 +0400

    Add parsing response from server to structured array of stops.

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {StopService} from '../../services/stop.service';
 import {Stop} from '../../models/Stop';
 import {StopInfo} from '../../models/StopInfo';
@@ -17,39 +17,38 @@ export class MainPageComponent implements OnInit {
 
   constructor(
     private stopService: StopService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
   }
+
   getStop(input) {
     console.log(input.value);
     this.stopService.getStopInfo(input.value).subscribe((res) => {
       console.log(res);
       let stops = [];
-      let routes = [];
-      let dateRoutes = [];
       this.generalName = res.name;
-      for(let stop of res.stops) {
-
-        if(stop.routes) {
+      for (let stop of res.stops) {
+             let routes = [];
+        if (stop.routes) {
           for (let route of stop.routes) {
-
-             for(let time of route.stop_times) {
-               let dateRoute = new DateRoute(time.shape, time.departure_timestamp, time.departure_time);
-                dateRoutes.push(dateRoute);
-                console.log(dateRoutes);
-             }
+             let dateRoutes = [];
+            for (let time of route.stop_times) {
+              let dateRoute = new DateRoute(time.shape, time.departure_timestamp, time.departure_time);
+              dateRoutes.push(dateRoute);
+            }
             let Route1 = new Route(route.name, dateRoutes);
-             console.log(routes);
-             routes.push(Route1);
-             }
+            routes.push(Route1);
           }
+        }
         let Stop1 = new Stop(stop.name, routes);
         stops.push(Stop1);
-        }
+      }
       console.log(stops);
     });
   }
+
   getValue(input) {
     this.inputValue = input.value;
   }
