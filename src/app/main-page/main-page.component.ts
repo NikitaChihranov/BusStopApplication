e58d1e71b64f7108610a53b5a462b136e6ce6923
nikitaChihranov <nikita.chihranov@gmail.com>
2019-02-25 04:13:43 +0400

    Add parsing stops to array of stopInfo.

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -4,6 +4,7 @@ import {Stop} from '../../models/Stop';
 import {StopInfo} from '../../models/StopInfo';
 import {Route} from '../../models/Route';
 import {DateRoute} from '../../models/DateRoute';
+import {StopSchedule} from '../../models/StopSchedule';
 
 @Component({
   selector: 'app-main-page',
@@ -13,7 +14,8 @@ import {DateRoute} from '../../models/DateRoute';
 export class MainPageComponent implements OnInit {
   inputValue = '';
   generalName = '';
-  stops: StopInfo[];
+  stopInfoArray: StopInfo[] = [];
+
 
   constructor(
     private stopService: StopService
@@ -24,16 +26,15 @@ export class MainPageComponent implements OnInit {
   }
 
   getStop(input) {
-    console.log(input.value);
     this.stopService.getStopInfo(input.value).subscribe((res) => {
       console.log(res);
       let stops = [];
       this.generalName = res.name;
       for (let stop of res.stops) {
-             let routes = [];
+        let routes = [];
         if (stop.routes) {
           for (let route of stop.routes) {
-             let dateRoutes = [];
+            let dateRoutes = [];
             for (let time of route.stop_times) {
               let dateRoute = new DateRoute(time.shape, time.departure_timestamp, time.departure_time);
               dateRoutes.push(dateRoute);
@@ -46,6 +47,18 @@ export class MainPageComponent implements OnInit {
         stops.push(Stop1);
       }
       console.log(stops);
+      for (let stop of stops) {
+        let stopSchedules = [];
+        for (let route of stop.routes) {
+          for (let eachStop of route.stop_times) {
+            let stopTime = new StopSchedule(route.name, eachStop.departure_timestamp);
+            stopSchedules.push(stopTime);
+          }
+        }
+        let newStopInfo = new StopInfo(stop.name, stopSchedules);
+        this.stopInfoArray.push(newStopInfo);
+      }
+      console.log(this.stopInfoArray);
     });
   }
 
